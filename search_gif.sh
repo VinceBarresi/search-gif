c6102c4e672b97ef92ba6cadfd2f9aed608709e6
Vincent Barresi <vincent.barresi@viacom.com>
2018-01-30 10:14:12 -0500

    replace spaces with dashes in input to pass to search query

--- search_gif.sh
+++ search_gif.sh
@@ -5,9 +5,9 @@ function log {
 function get_gif {
   echo "Enter gif search query ^:"
   read input
-  input=$input
+  manipulatedInput=${input// /-}
   echo "searching giphy with entered query"
-  url="http://api.giphy.com/v1/gifs/search?q=${input}&api_key=dc6zaTOxFJmzC&limit=1"
+  url="http://api.giphy.com/v1/gifs/search?q=${manipulatedInput}&api_key=dc6zaTOxFJmzC&limit=1"
   gif=$(curl $url | jq -r '.data[0].url')
   echo gif=${gif// /+}
   gif=$gif/fullscreen
